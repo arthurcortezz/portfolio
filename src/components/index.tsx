9a460edc1bb0e3ae361c808488aa682d67677480
arthurcortezz <arthurcortez2@gmail.com>
2022-08-29 11:54:35 -0300

    alterado pagina principal

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,6 +1,6 @@
-export { default as SideBar } from "./navegacao/sideBarTutorial";
-export { default as PaginaNova } from "../containers/PaginaNova";
-export { default as PaginaVazia } from "../containers/templates/PaginaVazia";
+export { default as SideBar } from "./navigation/sideBarTutorial";
+export { default as PaginaNova } from "../containers/NewPage";
+export { default as PaginaVazia } from "../containers/templates/EmptyPage";
 export { default as VideoPage } from "./pages/videoPage";
 export { default as ManutencaoPage } from "./pages/manutencao";
 export { default as FooterPreview } from "../components/footer/footerPage";
---
+++ src/pages/manager.tsx
@@ -0,0 +1,9 @@
+import { Flex, Text } from "@chakra-ui/react";
+
+export default function Menu() {
+  <>
+    <Flex w={"100%"}>
+      <Text fontWeight={600}> Hello World !</Text>
+    </Flex>
+  </>;
+}
