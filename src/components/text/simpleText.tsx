72d6d37b94111e963a8172de0368a9142f2c6dee
Arthur Cortez <arthurcortez2@gmail.com>
2022-09-19 13:57:22 -0300

    fix

--- src/components/text/simpleText.tsx
+++ src/components/text/simpleText.tsx
@@ -1,26 +1,23 @@
 import { Flex, Text } from "@chakra-ui/react";
 
 interface Props {
-  text: string[] | string;
+  text: string;
   size?: number;
   color?: string;
   w?: string;
+  positionFlex?: "relative" | "absolute";
 }
-export default function SimpleText({ w, color, text, size }: Props) {
+export default function SimpleText({ w, color, text, size, positionFlex }: Props) {
   let string = "";
   let array: any = [];
   typeof text === "string" ? (string = text) : (array = text);
   return (
     <>
-      {array &&
-        array.map((item: any, key: any) => (
-          <Flex key={key} direction={"column"} w={"100%"} textAlign={"left"}>
-            <Text color={color ? color : "black"} fontSize={size ? size : 18} w={w ? w : "100%"}>
-              {item}
-            </Text>
-          </Flex>
-        ))}
-      <Text color={color ? color : "black"} fontSize={size ? size : 18} w={w ? w : "100%"}>
+      <Text
+        position={positionFlex ? positionFlex : "relative"}
+        color={color ? color : "black"}
+        fontSize={size ? size : 18}
+        w={w ? w : "100%"}>
         {string && string}
       </Text>
     </>
